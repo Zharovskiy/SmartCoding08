f5737f47b231c09e3e993f318927107998cf503a
Yurii <jarovski08@gmail.com>
2024-02-22 17:46:27 +0200

    connecting files JavaScript

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import * as header from './js/header.js';
+import * as darkTheme from './js/dark-theme.js';
+import * as categories from './js/categories.js';
+import * as supportUkraine from './js/support-ukraine';
+import * as homePage from './js/homePage.js';
+import * as shoppingList from './js/shopping-list.js';
+import * as menu from './js/menu.js';
+import * as popUp from './js/pop-up.js';
