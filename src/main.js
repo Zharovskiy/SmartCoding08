c31673db9b6cb64c1775c51e4acf7ac0018d6e03
Yurii <jarovski08@gmail.com>
2024-02-23 20:06:21 +0200

    add category

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import * as header from './js/header.js';
 import * as categories from './js/categories.js';
 import * as supportUkraine from './js/support-ukraine';
 import * as homePage from './js/homePage.js';
+import * as category from './js/category.js'
 import * as shoppingList from './js/shopping-list.js';
 import * as menu from './js/menu.js';
 import * as popUp from './js/pop-up.js';
