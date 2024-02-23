43fab96cdb08416901304ea8978279b3940388ad
Yurii <jarovski08@gmail.com>
2024-02-23 14:36:00 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import * as header from './js/header.js';
-import * as darkTheme from './js/dark-theme.js';
 import * as categories from './js/categories.js';
 import * as supportUkraine from './js/support-ukraine';
 import * as homePage from './js/homePage.js';
