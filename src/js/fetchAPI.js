a7c06719f4e0f207252f4099a220775ad938abda
Kateryna1812 <super.katuysha2013@gmail.com>
2024-02-23 19:39:00 +0200

    commit-01

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,1 +1,25 @@
 import axios from "axios";
+
+export class BooksApi {
+    constructor() {}
+    booksAllCategories() {
+        axios.defaults.baseURL = 'https://books-backend.p.goit.global/books';
+        return axios.get('/category-list');
+    }
+    booksPopular() {
+        axios.defaults.baseURL = 'https://books-backend.p.goit.global/books';
+        return axios.get('/top-books');
+    }
+    booksCategorySeparate(selectedCategory) {
+        axios.defaults.baseURL = 'https://books-backend.p.goit.global/books';
+        return axios.get('/category', {
+            params: {
+                category: selectedCategory,
+            }
+        });
+    }
+    bookId(bookId) {
+        axios.defaults.baseURL = 'https://books-backend.p.goit.global/books';
+        return axios.get(bookId);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -5,3 +5,15 @@ import * as homePage from './js/homePage.js';
 import * as shoppingList from './js/shopping-list.js';
 import * as menu from './js/menu.js';
 import * as popUp from './js/pop-up.js';
+import { BooksApi } from './js/fetchAPI.js';
+const booksApi = new BooksApi();
+async function loadData () {
+    // loadBtn.insertAdjacentHTML('afterbegin', '<span class="loader"></span>');
+    try {
+      const response = await booksApi.bookId('643282b1e85766588626a0dc');
+      console.log(response)
+    }
+    catch (error) {
+    }
+  }
+loadData()
