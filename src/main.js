a4d9e3644a7f7b96cb8b8b4a1f47efa2b2173155
Yurii <jarovski08@gmail.com>
2024-02-24 12:55:01 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,15 +6,3 @@ import * as category from './js/category.js'
 import * as shoppingList from './js/shopping-list.js';
 import * as menu from './js/menu.js';
 import * as popUp from './js/pop-up.js';
-import { BooksApi } from './js/fetchAPI.js';
-const booksApi = new BooksApi();
-async function loadData () {
-    // loadBtn.insertAdjacentHTML('afterbegin', '<span class="loader"></span>');
-    try {
-      const response = await booksApi.bookId('643282b1e85766588626a0dc');
-      console.log(response)
-    }
-    catch (error) {
-    }
-  }
-loadData()
