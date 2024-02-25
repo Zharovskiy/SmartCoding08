ed722d6ea0e413d929f3f8ce78ca1cc55e731f61
VikoTory <143288265+VikoTory@users.noreply.github.com>
2024-02-25 15:56:07 +0200

    click-category-consol

--- src/js/categories.js
+++ src/js/categories.js
@@ -9,16 +9,38 @@ const categorySelectors = {
 
 function categoryMarkup(data) {
   const result = data
-    .map(item => `<li class='category-item'>${item.list_name}</li>`)
+    .map(item =>
+      `<li data-category="${item.list_name}" class='category-item'>${item.list_name}</li>`)
     .join('');
   return result;
+}
+
+async function loadCategory(categoryName) {
+  console.log('Loading category:', categoryName);
+  try {
+    const categoryData = await backendAPI.getCategoryList(categoryName);
+    renderCategoryPage(categoryData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function renderCategoryPage(data) {
+  
 }
 
 (async () => {
   try {
     const categoryData = await backendAPI.getCategoryList();
     const markup = categoryMarkup(categoryData);
-    categorySelectors.categoryList.insertAdjacentHTML('beforeend', markup);
+    categorySelectors.categoryList.innerHTML = markup;
+    
+    categorySelectors.categoryList.addEventListener('click', (event) => {
+      if (event.target.classList.contains('category-item')) {
+        const categoryName = event.target.dataset.category;
+        loadCategory(categoryName);
+      }
+    });
   } catch (error) {
     console.log(error);
   }
