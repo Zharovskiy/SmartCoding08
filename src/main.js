691d7086040a99e03818a7c1638574afa3e730cf
Yurii <jarovski08@gmail.com>
2024-02-25 23:38:54 +0200

    header

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import * as header from './js/header.js';
 import * as categories from './js/categories.js';
 import * as supportUkraine from './js/support-ukraine';
 import * as homePage from './js/homePage.js';
-import loadBooksCategory from './js/category.js';
 import * as shoppingList from './js/shopping-list.js';
 import * as menu from './js/menu.js';
 import * as popUp from './js/pop-up.js';
