1eab88c6da3525654405f9be7b9f7626d5a4fd43
MediukIV <143748068+MediukIV@users.noreply.github.com>
2024-02-25 12:45:20 +0200

    add_styles_homePage

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,28 +1,29 @@
 import backendAPI from "./fetchAPI";
 
-const categoriesBooksList = document.querySelector('.categories-books-list');
-
-const renderBooks = async () => {
+export const renderBestBooks = async () => {
+    const bestBooksList = document.querySelector('.bestsellers-list');
     try {
         const bestSellersData = await backendAPI.getBestSellers();
         const markup = bestSellersData.map(({ books, list_name }) => {
-            return `<li class="categories-item">${list_name}
-                <ul class="categories-books-list"> 
-                    ${books.map(({ book_image, title, author }) => {
-                        return `<li class="categories-item">
-                            <img class="categories-img" src='${book_image}' alt="" />
-                            <h3 class="categories-book-title">${title}</h3>
-                            <p class="categories-book-author">${author}</p>
+            return `<li class="bestsellers-item">
+             <h2 class="bestsellers-category-title">${list_name}</h2>
+                <ul class="bestsellers-books-list"> 
+                    ${books.map(({ book_image, title, author }) => { // if(){}else{}
+                        return `<li class="bestsellers-books-item">
+                            <img class="bestsellers-books-img" src='${book_image}' alt="" />
+                            <h3 class="bestsellers-book-title">${title}</h3>
+                            <p class="bestsellers-book-author">${author}</p>
                         </li>`}).join('\n')}
                 </ul>
-                <button class="categories-btn" type="button">See more</button>
+                <button class="bestsellers-btn" type="button">See more</button>
             </li>`;        
         }).join('\n');
         
-        categoriesBooksList.insertAdjacentHTML('beforeend', markup);
+        bestBooksList.insertAdjacentHTML('beforeend', markup);
+
     } catch (error) {
         console.error("Error fetching best sellers:", error);
     }
 }
 
-renderBooks();
+renderBestBooks();
