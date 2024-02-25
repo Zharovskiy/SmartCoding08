39b9073b79fe23c31400bce28668a67fcc5010c7
Yurii <jarovski08@gmail.com>
2024-02-25 20:46:41 +0200

    fix style

--- src/js/category.js
+++ src/js/category.js
@@ -10,20 +10,20 @@ export default async function loadBooksCategory (category) {
 
 function renderBooks (data) {
     const markupCard = data.map(({_id, book_image, title, author}) => 
-        `<li class="book-card" id="${_id}">
-        <div class="box-overlay">
-            <img class="book-image" src="${book_image}" alt="Book">
-            <p class="overlay">Quick view</p>
+        `<li class="category-book-card" id="${_id}">
+        <div class="category-box-overlay">
+            <img class="category-book-image" src="${book_image}" alt="Book">
+            <p class="category-overlay">Quick view</p>
         </div>
-        <div class="book-text">
-            <h3 class="book-title">${title}</h3>
-            <p class="book-description">${author}</p>
+        <div class="category-book-text">
+            <h3 class="category-book-title">${title}</h3>
+            <p class="category-book-description">${author}</p>
         </div>
         </li>`).join('');
 
     const markupBooks = 
-        `<h2 class="page-title">Hardcover<span class="titel-color"> Fiction</span></h2>
-        <ul class="books">
+        `<h2 class="category-page-title">Hardcover<span class="category-titel-color"> Fiction</span></h2>
+        <ul class="category-books">
         ${markupCard}
         </ul>`;
     // видалити лоадер
