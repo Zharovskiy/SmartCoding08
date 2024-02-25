c93f80925fb57508f6c0b6176f556d6207c7066e
Yurii <jarovski08@gmail.com>
2024-02-25 22:55:44 +0200

    favicon

--- src/main.js
+++ src/main.js
@@ -7,35 +7,36 @@ import * as shoppingList from './js/shopping-list.js';
 import * as menu from './js/menu.js';
 import * as popUp from './js/pop-up.js';
 
+// Контейнер для рендеру сторінок
 export const pageContainer = document.querySelector('.home-page');
 
 // Виклик по Кнопка лого і Кнопка Номе
-export function firstLoad () {
+// export function firstLoad () {
     // Видалити все з контейнера homePage
     // if() {
 // Рендер всіх категорій в сайд барі через перевірку чи секція наявна
     // } 
     // Саппорт Україна
     // Бестселлерс
-}
+// }
 
 // Виклик по натисканню на кнопку шоппінг ліст
-export function loadShoppingList () {
+// export function loadShoppingList () {
     // Видалити все з контейнера homePage
     // Видалити секцію категорії в сайдбарі
     // Рендер Шоппінг ліст
-}
+// }
 
 // Виклик сторінки категорії книг
-export function loadCategory (category) {
+// export function loadCategory (category) {
     // Видалити все з контейнера homePage
     // if() {
     // Рендер всіх категорій в сайд барі через перевірку чи секція наявна
     // }
-}
+// }
 
 // Перше завантаження виклик
-firstLoad();
+// firstLoad();
 
 
 // loadBooksCategory('Hardcover Fiction');
