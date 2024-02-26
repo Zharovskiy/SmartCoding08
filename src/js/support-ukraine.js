6b45b210df9260e79e746b1e4a0d27e518474eae
hiillel <vicky.hillel26@gmail.com>
2024-02-27 00:54:40 +0100

    fixed sequence in the array support ukraine

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -11,18 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
       img: null,
     },
     {
-      title: 'UNITED24',
-      url: 'https://u24.gov.ua/uk',
-      img: null,
-    },
-    {
       title: 'International Medical Corps',
       url: 'https://internationalmedicalcorps.org/country/ukraine/',
-      img: null,
-    },
-    {
-      title: 'Medicins Sans Frontieres',
-      url: 'https://www.msf.org/ukraine',
       img: null,
     },
     {
@@ -36,13 +26,23 @@ document.addEventListener('DOMContentLoaded', function () {
       img: null,
     },
     {
+      title: 'Serhiy Prytula Charity Foundation',
+      url: 'https://prytulafoundation.org/en',
+      img: null,
+    },
+    {
+      title: 'Medicins Sans Frontieres',
+      url: 'https://www.msf.org/ukraine',
+      img: null,
+    },
+    {
       title: 'World vision',
       url: 'https://www.wvi.org/emergencies/ukraine',
       img: null,
     },
     {
-      title: 'Serhiy Prytula Charity Foundation',
-      url: 'https://prytulafoundation.org/en',
+      title: 'UNITED24',
+      url: 'https://u24.gov.ua/uk',
       img: null,
     },
   ];
@@ -55,22 +55,22 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  var fundList = document.getElementById("fundList");
-  var fundItems = fundList.getElementsByClassName("fund-item");
-  var fundBtn = document.querySelector(".fund-btn");
+document.addEventListener('DOMContentLoaded', function () {
+  var fundList = document.getElementById('fundList');
+  var fundItems = fundList.getElementsByClassName('fund-item');
+  var fundBtn = document.querySelector('.fund-btn');
 
   // Функция для скрытия всех элементов списка
   function hideAllItems() {
     for (var i = 0; i < fundItems.length; i++) {
-      fundItems[i].style.display = "none";
+      fundItems[i].style.display = 'none';
     }
   }
 
   // Функция для показа первых шести элементов
   function showFirstSixItems() {
     for (var i = 0; i < 6; i++) {
-      fundItems[i].style.display = "block";
+      fundItems[i].style.display = 'block';
     }
   }
 
@@ -79,29 +79,29 @@ document.addEventListener("DOMContentLoaded", function() {
   showFirstSixItems();
 
   // При нажатии на кнопку отображаем следующие шесть элементов, пролистываясь
-  fundBtn.addEventListener("click", function() {
-    var isHidden = fundItems[0].style.display === "none";
+  fundBtn.addEventListener('click', function () {
+    var isHidden = fundItems[0].style.display === 'none';
 
     if (isHidden) {
       // Показываем верхние три элемента
       for (var i = 0; i < 3; i++) {
-        fundItems[i].style.display = "block";
+        fundItems[i].style.display = 'block';
       }
       // Скрываем следующие шесть элементов
       for (var i = 6; i < 12; i++) {
         if (fundItems[i]) {
-          fundItems[i].style.display = "none";
+          fundItems[i].style.display = 'none';
         }
       }
     } else {
       // Скрываем верхние три элемента
       for (var i = 0; i < 3; i++) {
-        fundItems[i].style.display = "none";
+        fundItems[i].style.display = 'none';
       }
       // Показываем следующие шесть элементов
       for (var i = 6; i < 12; i++) {
         if (fundItems[i]) {
-          fundItems[i].style.display = "block";
+          fundItems[i].style.display = 'block';
         }
       }
     }
