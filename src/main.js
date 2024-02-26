57cb8d903bfe88b6812cb70a04e9d231c14568bd
Yurii <jarovski08@gmail.com>
2024-02-27 01:40:51 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import * as header from './js/header.js';
 import * as categories from './js/categories.js';
-// import * as supportUkraine from './js/support-ukraine';
+import * as supportUkraine from './js/support-ukraine';
 import * as homePage from './js/homePage.js';
 import * as shoppingList from './js/shopping-list.js'
 import * as menu from './js/menu.js';
