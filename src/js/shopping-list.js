c4119a9bca1e400fcfb494b1211f128851ac7f6a
Yurii <jarovski08@gmail.com>
2024-02-26 03:29:51 +0200

    shoppingList js

---
+++ src/js/shopping-list.js
@@ -0,0 +1,59 @@
+import { pageContainer } from "../main";
+import { LocalStorage } from './localStorage.js';
+const localStorage = new LocalStorage();
+
+// прослуховування кнопки shopping List в хедері
+const shopBtn = document.getElementById('shopping-link');
+shopBtn.addEventListener('click', loadShoppingList);
+
+function loadShoppingList () {
+    pageContainer.innerHTML = '';
+
+    // Видалення секції категорій в сайдбарі
+    const categoryList = document.querySelector('.sidebar-category-container');
+    categoryList.remove();
+
+    // додати лоадер
+    // Видалення слухача категорій книг з сайдбару 
+
+    // Видалення слухача карточок книжок
+
+    const dataShop = localStorage.getProducts();
+    renderShoppingList(dataShop);
+}
+
+function renderShoppingList(dataShop) {
+    const markupCard = dataShop.map(({_id, book_image, title, author}) => 
+        `<li class="category-book-card" id="${_id}">
+        <div class="category-box-overlay">
+            <img class="category-book-image" src="${book_image}" alt="Book">
+            <p class="category-overlay">Quick view</p>
+        </div>
+        <div class="category-book-text">
+            <h3 class="category-book-title">${title}</h3>
+            <p class="category-book-description">${author}</p>
+        </div>
+        </li>`).join('');
+
+    const markupPageShop = 
+        `<h2 class="category-page-title">Shopping<span class="category-titel-color"> List</span></h2>
+        <ul class="category-books">
+        ${markupCard}
+        </ul>`;
+    // видалити лоадер
+    pageContainer.insertAdjacentHTML('afterbegin', markupPageShop);
+    
+    // додавання слухача для видалення товару
+    pageContainer.addEventListener('click', delBookFromStorage);
+}
+
+function delBookFromStorage (event) {
+    const elemID = event;
+    console.log(elemID);
+    // дістати елемент на який натиснули з id
+    // localStorage.putProducts({id: });
+    // const delBookMarkup = document.getElementById(id);
+
+}
+    
+export default { shopBtn };
--- src/main.js
+++ src/main.js
@@ -2,62 +2,10 @@ import * as header from './js/header.js';
 import * as categories from './js/categories.js';
 import * as supportUkraine from './js/support-ukraine';
 import * as homePage from './js/homePage.js';
-import * as shoppingList from './js/shopping-list.js';
+import shoppingList from './js/shopping-list.js'
 import * as menu from './js/menu.js';
 import * as popUp from './js/pop-up.js';
 
 // Контейнер для рендеру сторінок
 export const pageContainer = document.querySelector('.home-page');
-
-// Виклик по Кнопка лого і Кнопка Номе
-// export function firstLoad () {
-    // Видалити все з контейнера homePage
-    // if() {
-// Рендер всіх категорій в сайд барі через перевірку чи секція наявна
-    // } 
-    // Саппорт Україна
-    // Бестселлерс
-// }
-
-// Виклик по натисканню на кнопку шоппінг ліст
-// export function loadShoppingList () {
-    // Видалити все з контейнера homePage
-    // Видалити секцію категорії в сайдбарі
-    // Рендер Шоппінг ліст
-// }
-
-// Виклик сторінки категорії книг
-// export function loadCategory (category) {
-    // Видалити все з контейнера homePage
-    // if() {
-    // Рендер всіх категорій в сайд барі через перевірку чи секція наявна
-    // }
-// }
-
-// Перше завантаження виклик
-// firstLoad();
-
-
-// loadBooksCategory('Hardcover Fiction');
-
-
-
-// Для тестування localStorage
-// import { LocalStorage } from './js/localStorage.js';
-// const localStorage = new LocalStorage();
-
-// const books = {
-//   id: '1',
-//   urlImg: '',
-//   name: '',
-//   category: '',
-//   description: '',
-//   author: '',
-//   urlMarket: [] 
-// }
-
-
-
-// const a = localStorage.getProducts();
-// console.log(a);
 
