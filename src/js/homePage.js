497632a133fb76dfd3576df0e93e19d804dce389
JuliaNedilchak <julianedilchak@gmail.com>
2024-02-26 17:25:50 +0000

    menu

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -29,30 +29,38 @@ export function renderBestBooks(bestBooks) {
   const bestBooksContainer = document.querySelector('.bestsellers-container');
   bestBooksContainer.innerHTML = '';
   renderTitle('.bestsellers-container', 'Best Sellers Books');
-  bestBooksContainer.insertAdjacentHTML('beforeend', `<ul class="bestsellers-list"></ul>`);
+  bestBooksContainer.insertAdjacentHTML(
+    'beforeend',
+    `<ul class="bestsellers-list"></ul>`
+  );
   const bestBooksList = document.querySelector('.bestsellers-list');
-  
-    const markup = bestBooks.map(({ books, list_name }) => {
+
+  const markup = bestBooks
+    .map(({ books, list_name }) => {
       return `
             <li class="bestsellers-item">
              <h2 class="bestsellers-category-title">${list_name}</h2>
                 <ul class="bestsellers-books-list"> 
-                    ${books.map(({ book_image, title, author }) => {return bookTemplate({ book_image, title, author })
-                        }).join('\n')}
+                    ${books
+                      .map(({ book_image, title, author }) => {
+                        return bookTemplate({ book_image, title, author });
+                      })
+                      .join('\n')}
                 </ul>
                 <button class="bestsellers-btn" type="button data-category="${list_name}">See more</button>
-            </li>`;        
-        }).join('\n');
+            </li>`;
+    })
+    .join('\n');
 
-        bestBooksList.insertAdjacentHTML('beforeend', markup);
+  bestBooksList.insertAdjacentHTML('beforeend', markup);
 }
 
 export async function BestsellersBooks() {
-    try {
-        const bestSellersData = await backendAPI.getBestSellers();
-        return renderBestBooks(bestSellersData);
-    } catch (error) {
-        console.error("Error fetching best sellers:", error);
-    }
+  try {
+    const bestSellersData = await backendAPI.getBestSellers();
+    return renderBestBooks(bestSellersData);
+  } catch (error) {
+    console.error('Error fetching best sellers:', error);
+  }
 }
 BestsellersBooks();
