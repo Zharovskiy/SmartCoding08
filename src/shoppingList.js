295593ad7c697784a9efabbe344fccfbf1e93912
Yurii <jarovski08@gmail.com>
2024-02-27 11:30:41 +0200

    add files for page shopping list

---
+++ src/shoppingList.js
@@ -0,0 +1,10 @@
+import * as header from './js/header.js';
+import * as categories from './js/categories.js';
+import * as supportUkraine from './js/support-ukraine.js';
+import * as shoppingList from './js/shopping-list.js'
+import * as menu from './js/menu.js';
+import * as scrollUp from './js/scroll-up.js'
+
+// Контейнер для рендеру сторінок
+export const pageContainer = document.querySelector('.home-page');
+
