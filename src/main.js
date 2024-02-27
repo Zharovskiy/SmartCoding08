128812db6cdafb5df2d5857ff08bac77f3bc3876
Yurii <jarovski08@gmail.com>
2024-02-27 02:29:08 +0200

    popup

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import * as supportUkraine from './js/support-ukraine';
 import * as homePage from './js/homePage.js';
 import * as shoppingList from './js/shopping-list.js'
 import * as menu from './js/menu.js';
-import * as popUp from './js/pop-up.js';
+import * as popUp from './js/pop-up.js'
 import * as scrollUp from './js/scroll-up.js'
 
 // Контейнер для рендеру сторінок
