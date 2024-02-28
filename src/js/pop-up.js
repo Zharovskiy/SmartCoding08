80baafbd33051661f3ac03cda938ff956c134ec6
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-02-28 23:29:45 +0100

    pop-up-#js-modal

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,107 +1,96 @@
 import { LocalStorage } from './localStorage.js';
+import { backendAPI } from './fetchAPI.js'; 
+import { renderTitle, bookTemplate } from './homePage.js'; // Імпортуємо функції з homePage.js
 
-  const modal = document.getElementById('modal');
-  const closeButton = document.querySelector('.modal-close');
-  const addToCartButton = document.querySelector('.modal-add-to-cart');
-  const amazonLogo = document.querySelector('.icon-link-amazon');
-  const appleLogo = document.querySelector('.icon-link-apple');
-  const localStorageInstance = new LocalStorage();
+const localStorageUtil = new LocalStorage(); 
+const modal = document.getElementById('modal');
+const closeButton = document.querySelector('.modal-close');
+const addToCartButton = document.querySelector('.modal-add-to-cart');
+const modalShoppingBtn = document.querySelector('.modal-add-to-cart');
+const closeBtn = document.querySelector('.modal-close-btn');
 
-  function openModal() {
-    modal.style.display = 'block';
-    document.body.style.overflow = 'hidden';
-    document.documentElement.style.overflow = 'hidden'; // Блокування прокрутки
-  }
+export default function addListener() {
+    const bookLinks = document.querySelectorAll('.book-link');
+    bookLinks.forEach(bookLink =>
+        bookLink.addEventListener('click', onOpenModalWindow)
+    );
+}
 
-  function closeModal() {
-    modal.style.display = 'none';
-    document.body.style.overflow = 'auto';
-    document.documentElement.style.overflow = 'auto'; // Розблакування прокрутки
-  }
+async function onOpenModalWindow(event) {
+    event.preventDefault();
+    const selectedBookId = event.currentTarget.dataset.id;
+    try {
+        backendAPI.bookId = selectedBookId;
+        const resp = await backendAPI.fetchBookById();
+        const selectedBook = resp.data;
+        createBookMarkup(selectedBook);
+        modal.classList.remove('is-hidden'); // Показуємо модальне вікно
+        document.body.style.overflow = 'hidden'; // Забороняємо прокрутку тіла документа
+        closeButton.addEventListener('click', onCloseModalWindow);
+        window.addEventListener('keydown', onEsc);
+    } catch (error) {
+        console.error('Error occurred while processing data:', error);
+    }
+}
 
-  closeButton.addEventListener('click', closeModal);
+function createBookMarkup(book) {
+    const modalCardContent = document.querySelector('.modal-card');
+    modalCardContent.innerHTML = `
+        <img class="modal-book-cover" src="${book.book_image}" alt="Book Cover">
+        <div class="modal-blok-text">
+            <p class="modal-book-title">${book.title}</p>
+            <p class="modal-author">${book.author}</p>
+            <p class="modal-description">${book.description}</p>
+            <div class="modal-links">
+                <a href="${book.buy_links[0].url}" target="_blank" rel="noopener noreferrer">
+                    <picture class="icon-link-amazon">
+                        <source srcset="${book.buy_links[0].webp}" type="image/webp"/>
+                        <source srcset="${book.buy_links[0].png}" type="image/png"/>
+                        <img src="${book.buy_links[0].png}" alt="Amazon"/>
+                    </picture>
+                </a>
+                <a href="${book.buy_links[1].url}" target="_blank" rel="noopener noreferrer">
+                    <picture class="icon-link-apple">
+                        <source srcset="${book.buy_links[1].webp}" type="image/webp"/>
+                        <source srcset="${book.buy_links[1].png}" type="image/png"/>
+                        <img src="${book.buy_links[1].png}" alt="Apple Books"/>
+                    </picture>
+                </a>
+            </div>
+        </div>`;
+    
+    modalShoppingBtn.addEventListener('click', () => onUpdateShopList(book));
+}
 
+async function onUpdateShopList(bookData) {
+    const storage = await localStorageUtil.getProducts();
+    const isBookAdded = storage.some(book => book.id === bookData.id);
+    if (!isBookAdded) {
+        localStorageUtil.putProducts(bookData); 
+        addBtn();
+    } else {
+        localStorageUtil.removeProduct(bookData.id); 
+        removeBtn();
+    }
+}
 
+function addBtn() {
+    modalShoppingBtn.textContent = 'Remove from Shopping List';
+}
 
-// document.addEventListener('DOMContentLoaded', function() {
+function removeBtn() {
+    modalShoppingBtn.textContent = 'Add to Shopping List';
+}
 
+function onEsc(event) {
+    if (event.key === 'Escape') {
+        onCloseModalWindow();
+    }
+}
 
-//   modal.addEventListener('click', function(event) {
-//     if (event.target === modal) {
-//       closeModal();
-//     }
-//   });
-
-//   document.addEventListener('keydown', function(event) {
-//     if (event.key === 'Escape') {
-//       closeModal();
-//     }
-//   });
-
-//   // Додайте обробники подій для кожного логотипу
-//   amazonLogo.addEventListener('click', function(event) {
-//     event.preventDefault(); // Зупиніть перехід за замовчуванням
-//     window.open(this.href, '_blank'); // Відкриття посилання у новій вкладці
-//   });
-
-//   appleLogo.addEventListener('click', function(event) {
-//     event.preventDefault(); // Зупиніть перехід за замовчуванням
-//     window.open(this.href, '_blank'); // Відкриття посилання у новій вкладці
-//   });
-
-//   // Функція для оновлення вмісту модального вікна
-//   function updateModal(book) {
-//     const bookCover = modal.querySelector('.modal-book-cover');
-//     const bookTitle = modal.querySelector('.modal-book-title');
-//     const author = modal.querySelector('.modal-author');
-//     const description = modal.querySelector('.modal-description');
-//     const links = modal.querySelector('.modal-links');
-
-//     bookCover.src = book.coverImageUrl;
-//     bookTitle.textContent = book.title;
-//     author.textContent = '' + book.author;
-//     description.textContent = book.description;
-
-//     // Очистити попередні посилання
-//     links.innerHTML = '';
-
-//     // Додати посилання на торгівельні майданчики
-//     book.links.forEach(function(link) {
-//       const anchor = document.createElement('a');
-//       anchor.href = link.url;
-//       anchor.target = '_blank';
-
-//       // Додайте зображення логотипу замість тексту посилання
-//       const logoImg = document.createElement('img');
-//       logoImg.src = link.logoUrl;
-//       logoImg.alt = link.name;
-//       anchor.appendChild(logoImg);
-
-//       links.appendChild(anchor);
-//     });
-
-//     // Перевірка, чи книга вже є в кошику
-//     const isInShoppingList = localStorageInstance.checkProduct(book.id).hasProd;
-
-//     if (isInShoppingList) {
-//       addToCartButton.textContent = 'Remove from Shopping List';
-//     } else {
-//       addToCartButton.textContent = 'Add to Shopping List';
-//     }
-//   }
-
-//   // Пошук книги
-//   const exampleBook = {
-//     id: 1,
-//     coverImageUrl: 'https://example.com/book-cover.jpg',
-//     title: 'Book Title',
-//     author: 'John Doe',
-//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-//     links: [
-//       { logoUrl: '/src/images/pop-up/amazon.svg', url: 'https://www.amazon.com' },
-//       { logoUrl: '/src/images/pop-up/apple.svg', url: 'https://books.apple.com' }
-//     ]
-//   };
-
-//   updateModal(exampleBook);
-// });
+function onCloseModalWindow() {
+    modal.classList.add('is-hidden'); // Приховуємо модальне вікно
+    document.body.style.overflow = 'visible'; // Відновлюємо прокрутку тіла документа
+    closeButton.removeEventListener('click', onCloseModalWindow);
+    window.removeEventListener('keydown', onEsc);
+}
