3bf2d377a615cd78b08d6c1ff5864235c1f0dd6a
Yurii <jarovski08@gmail.com>
2024-02-28 18:38:01 +0200

    1.0.1

--- src/js/2-shopping-list.js
+++ src/js/2-shopping-list.js
@@ -1,6 +1,6 @@
 import * as header from './header.js';
 import * as supportUkraine from './support-ukraine.js';
 import * as shoppingList from './shopping-list.js'
-// import * as menu from './menu.js';
+import * as menu from './menu.js';
 import * as scrollUp from './scroll-up.js'
 
