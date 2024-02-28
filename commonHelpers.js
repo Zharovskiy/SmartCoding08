e309188e2d10075cf3e270dc6b71b73af79ed256
Zharovskiy <143278195+Zharovskiy@users.noreply.github.com>
2024-02-28 06:48:19 +0000

    Deploying to gh-pages from @ Zharovskiy/SmartCoding08@970d8e5b45299e9a65886add28d4ce10a1e6f5ce 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,11 +1,27 @@
-import"./assets/scroll-up-b5552339.js";class l{constructor(){this.keyName="shopping-list"}getProducts(){const e=localStorage.getItem(this.keyName);return e!==null?JSON.parse(e):[]}checkProduct(e){let t=this.getProducts(),r=t.findIndex(n=>n.id==e);const s={hasProd:!1,index:r,arrProd:t};return r!==-1&&(s.hasProd=!0),s}putProducts(e){let{hasProd:t,index:r,arrProd:s}=this.checkProduct(e.id);t!==!0?s.push(e):s.splice(r,1),localStorage.setItem(this.keyName,JSON.stringify(s))}}const c=new l,i=c.getProducts();function p(){const o=document.getElementById("bookList");if(o.innerHTML="",i.length===0||!c){o.innerHTML=`
+import"./assets/scroll-up-9d931d40.js";class r{constructor(){this.keyName="shopping-list"}getProducts(){const e=localStorage.getItem(this.keyName);return e!==null?JSON.parse(e):[]}checkProduct(e){let s=this.getProducts(),t=s.findIndex(l=>l.id==e);const o={hasProd:!1,index:t,arrProd:s};return t!==-1&&(o.hasProd=!0),o}putProducts(e){let{hasProd:s,index:t,arrProd:o}=this.checkProduct(e.id);s!==!0?o.push(e):o.splice(t,1),localStorage.setItem(this.keyName,JSON.stringify(o))}}const c=new r,a=c.getProducts();function n(){const i=document.getElementById("bookList");if(i.innerHTML="",a.length===0||!localStorage){i.innerHTML=`
             <li>
                 <picture>
                     <source srcset="../images/shopping-list/placeholder.webp" type="image/webp">
                     <source srcset="../images/shopping-list/placeholder.png" type="image/png">
-                    <source srcset="../images/shopping-list/placeholder2x.webp" type="image/webp">
-                    <source srcset="../images/shopping-list/placeholder2x.png" type="image/png">
                     <img src="../images/shopping-list/placeholder.png" alt="No Books Added">
                 </picture>
-            </li>`;return}i.forEach(e=>{const t=document.createElement("li");t.textContent=e,o.appendChild(t)})}p();
+            </li>`;return}a.map(s=>{const t=document.createElement("li");t.classList.add("book-card");const o=`
+            <img class="book-cover-img" src="${s.book_image}" alt="Book Cover">
+            <div class="book-info">
+                <h2 class="book-name-title">${s.title}</h2>
+                <p class="book-name-category">${s.list_name}</p>
+                <p class="book-short-description">${s.description}</p>
+                <div class="book-author-link">
+                    <p class="author-full-name">${s.author}</p>
+                    <a class="link" href="${s.amazon_product_url}">
+                        <img class="icon-link" src="../images/shopping-list/amazon-icon.svg" width="32px" height="11px" alt="Amazon">
+                    </a>
+                    <a class="link" href="${s.buy_links[1].url}">
+                        <img class="icon-link" src="../images/shopping-list/аpple-icon.svg" width="16px" height="16px" alt="Apple Books">
+                    </a>
+                </div>
+                <button class="remove-book">
+                    <img class="del-icon" src="../images/shopping-list/trash-03.svg" alt="Delete">
+                </button>
+            </div>`;return t.innerHTML=o,t}).forEach(s=>{i.appendChild(s)})}n();
 //# sourceMappingURL=commonHelpers.js.map
