87e40371ad0fffe7a23698c371cfb93378ec39f6
Yurii <jarovski08@gmail.com>
2024-02-29 07:27:49 +0200

    popup

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,52 +1,48 @@
 // -----------images----------
-import amazon from '../images/shopping/amazon.webp';
-import amazonpng from '../images/shopping/amazon.png';
-import apple from '../images/shopping/apple.webp';
-import applepng from '../images/shopping/apple.png';
-// import bookshop from '../images/shopping/bookshop.webp';
-// import bookshoppng from '../images/shopping/bookshop.png';
+import amazon from '../images/pop-up/amazon.webp';
+import amazonpng from '../images/pop-up/amazon.png';
+import apple from '../images/pop-up/books.webp';
+import applepng from '../images/pop-up/books.png';
 // -----------------------------
 import { LocalStorage } from './localStorage.js';
-// import { Notify } from 'notiflix';
-import { SwaggerAPI } from './swagger-api.js';
-// import { countBook } from './templates/shoppingListCounter';
-const fetchAPI = new SwaggerAPI();
-const backdrop = document.querySelector('.backdrop');
-const closeBtn = document.querySelector('.modal-close-btn');
-const modalShoppingBtn = document.querySelector('.modal-btn');
-const modalInfo = document.querySelector('.modal-info');
-const container = document.querySelector('.modal-book');
+import backendAPI from './fetchAPI';
+
+const localStor = new LocalStorage()
+const backdrop = document.querySelector('.modal');
+const closeBtn = document.querySelector('.modal-close');
+const modalShoppingBtn = document.querySelector('.modal-add-to-cart');
+const container = document.querySelector('.modal-card');
+
 let bookApi = {};
 export default function addListener() {
-  const bookContainer = document.querySelectorAll('.book-link');
+  const bookContainer = document.querySelectorAll('.book-category-item');
   bookContainer.forEach(book =>
     book.addEventListener('click', onOpenModalWindow)
   );
 }
+
 async function onOpenModalWindow(event) {
-  event.preventDefault();
-  document.body.style.overflow = 'hidden';
+  document.body.style.overflow = 'is-hidden';
   backdrop.classList.toggle('is-hidden');
   backdrop.addEventListener('click', onBackdrop);
   window.addEventListener('keydown', onEsc);
   closeBtn.addEventListener('click', onCloseModalWindow);
   try {
-    fetchAPI.bookId = event.currentTarget.dataset.id;
-    const resp = await fetchAPI.fetchBookById();
-    bookApi = resp.data;
+    bookApi.bookId = event.currentTarget.dataset.id;
+    const resp = await backendAPI.getBookDescription();
+    bookApi = resp;
+    console.log(resp);
     createBookMarkup(bookApi);
     setTimeout(() => {
       createShoppingBtn(bookApi);
     }, 0);
     modalShoppingBtn.addEventListener('click', onUpdateShopList);
   } catch {
-    Notify.failure(
-      'Oops! Something went wrong. We are sorry for the inconvenience. Please try again later.'
-    );
+    console.log('помилка') 
   }
 }
 async function createShoppingBtn(data) {
-  const storage = await localStorage.load('bookList');
+  const storage = await localStor.getProducts();
   if (!storage || storage.length === 0) {
     addBtn();
     return;
@@ -58,29 +54,7 @@ async function createShoppingBtn(data) {
     addBtn();
   }
 }
-function onUpdateShopList() {
-  const storage = localStorage.load('bookList');
-  const title = document.querySelector('.modal-book-name').textContent;
-  if (modalShoppingBtn.textContent === 'add to shopping list') {
-    localStorage.addBookToStorage(bookApi);
-    Notify.success('The book has been added to your shopping cart.')
-    removeBtn();
-    countBook();
-  } else {
-    storage.forEach((book, ind, arr) => {
-      if (book.title === title) {
-        return arr.splice(ind, 1);
-      }
-    });
-    localStorage.save('bookList', storage);
-    if (storage.length === 0) {
-      localStorage.remove('bookList');
-    }
-    countBook();
-    addBtn();
-    Notify.failure('The book has been removed from your shopping cart.')
-  }
-}
+
 function addBtn() {
   modalShoppingBtn.textContent = 'add to shopping list';
   modalInfo.style.display = 'none';
@@ -90,64 +64,28 @@ function removeBtn() {
   modalInfo.style.display = 'block';
 }
 function createBookMarkup(book) {
-  const markup = `<img
-class="modal-img"
-src=${book.book_image}
-alt="Book cover"
-/>
-<div class="modal-info-container">
-<p class="modal-book-name">${book.title}</p>
-<p class="modal-book-author">${book.author}</p>
-<p class="modal-list-name is-hidden">${book.list_name}</p>
-<p class="modal-book-desc">${book.description}
-</p>
-<div class="modal-icons-container">
-  <a href="${book.buy_links[0].url}" target="_blank" rel="noopener noreferrer"
-    ><picture class="modal-icon">
-      <source
-        srcset="${amazon}"
-        type="image/webp"
-      />
-      <source
-        srcset="${amazonpng}"
-        type="image/png"
-      />
-      <img
-        src="${amazonpng}"
-        alt="Amazon"
-      /> </picture
-  ></a>
-  <a href="${book.buy_links[1].url}" target="_blank" rel="noopener noreferrer"
-    ><picture class="modal-icon">
-      <source
-        srcset="${apple}"
-        type="image/webp"
-      />
-      <source
-        srcset="${applepng}"
-        type="image/png"
-      />
-      <img
-        src="${applepng}"
-        alt="Apple Books"
-      /> </picture
-  ></a>
-  <a href="${book.buy_links[4].url}" target="_blank" rel="noopener noreferrer"
-    ><picture class="modal-icon">
-      <source
-        srcset="${bookshop}"
-        type="image/webp"
-      />
-      <source
-        srcset="${bookshoppng}"
-        type="image/png"
-      />
-      <img
-        src="${bookshoppng}"
-        alt="Bookshop"
-      /></picture
-  ></a>
-</div>
+  const markup = `
+  <img class="modal-book-cover" src=${book.book_image} alt="Book cover"/>
+    <div class="modal-blok-text">
+      <p class="modal-book-title">${book.title}</p>
+      <p class="modal-author">${book.author}</p>
+      <p class="modal-description">${book.description}</p>
+    <div class="modal-links">
+    <a href="${book.buy_links[0].url}" target="_blank" rel="noopener noreferrer">
+      <picture class="modal-icon">
+        <source srcset="${amazon}" type="image/webp"/>
+        <source srcset="${amazonpng}" type="image/png"/>
+        <img src="${amazonpng}" alt="Amazon"/> 
+      </picture>
+    </a>
+    <a href="${book.buy_links[1].url}" target="_blank" rel="noopener noreferrer">
+      <picture class="modal-icon">
+        <source srcset="${apple}" type="image/webp"/>
+        <source srcset="${applepng}" type="image/png"/>
+        <img src="${applepng}" alt="Apple Books"/> 
+      </picture>
+    </a>
+  </div>
 </div>`;
   container.innerHTML = markup;
 }
