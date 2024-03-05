0f101f35dc817d330bec4cd74038f3286738b400
Zharovskiy <143278195+Zharovskiy@users.noreply.github.com>
2024-03-05 10:26:43 +0000

    Deploying to gh-pages from @ Zharovskiy/SmartCoding08@edf5c5e52792197d1777ba2d9be5f0dd72fe9f5a 🚀

--- assets/scroll-up-565243a6.js
+++ assets/scroll-up-565243a6.js
@@ -3,4 +3,4 @@
             <use href="${w}#scroll-up"></use>
         </svg>
     `,document.body.appendChild(o);function n(){window.scrollTo({top:0,behavior:"smooth"})}o.addEventListener("click",n),window.addEventListener("scroll",function(){window.scrollY>100?o.classList.add("show"):o.classList.remove("show")})});export{w as s};
-//# sourceMappingURL=scroll-up-a31c147e.js.map
+//# sourceMappingURL=scroll-up-565243a6.js.map
